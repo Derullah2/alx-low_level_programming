08992ef3992063edc4282ea88661d8f96a6619f3
Derullah2 <ewadyrogati98@gmail.com>
2023-03-16 14:22:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*
  * programming in c
  * main - main
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
