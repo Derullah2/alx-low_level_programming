f4b372e5fd65da8a7a2feac5cd1093055202c969
Derullah2 <ewadyrogati98@gmail.com>
2023-03-20 15:33:17 -0700

    alphabetic order

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main entry
+ *
+ * Return: Always Zero
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for  (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+
+
+	return (0);
+}
