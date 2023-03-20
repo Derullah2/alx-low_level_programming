9bbdde886bd2f3bcbdb8e4af6669c14746be7743
Derullah2 <ewadyrogati98@gmail.com>
2023-03-20 15:47:28 -0700

    alphabet witout q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+
+int a;
+
+	for (a = 97; a  <= 122; a++)
+	{
+		if (a == 101 || a == 113)
+		;
+		else
+		{
+			putchar(a);
+		}
+	}
+	putchar(10);
+	return (0);
+
+}
