d3d7dc6c6fcf3f423b9e2ebaba11eed6a87b0748
Derullah2 <ewadyrogati98@gmail.com>
2023-03-20 16:23:44 -0700

    backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+int a;
+
+	for (a = 122; a >= 97; --a)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
