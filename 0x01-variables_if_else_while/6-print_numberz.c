ea0645d09c3e2462bbad669911e0d968d896724a
Derullah2 <ewadyrogati98@gmail.com>
2023-03-20 16:07:23 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - mani ente
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+
+}
