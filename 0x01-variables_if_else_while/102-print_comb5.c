b9cc6ba693fdf11d92c235b6d1ed95ae3010b8a0
Derullah2 <ewadyrogati98@gmail.com>
2023-03-20 17:55:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	short a, b;
+
+	for (a = 0; b <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			if (a < b)
+			{
+				short a1 = a / 10;
+				short a2 = a % 10;
+				short b1 = b / 10;
+				short b2 = b % 10;
+
+				putchar(a1 + '0');
+				putchar(a2 + '0');
+				putchar(' ');
+				putchar(b1 + '0');
+				putchar(b2 + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
