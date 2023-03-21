e9b4bab7f59ca8c2a63d180a1e44de1f30c9aeb7
Derullah2 <ewadyrogati98@gmail.com>
2023-03-20 17:36:46 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Return: 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 1; m <= 9; m++)
+		{
+			if (n < m && n != m)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				if (n + m != 17)
+			{
+					putchar(',');
+					putchar('');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
