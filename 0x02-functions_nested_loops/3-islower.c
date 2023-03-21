d1ec2ca52d673570d0403959b445813c54a6d1bc
Derullah2 <ewadyrogati98@gmail.com>
2023-03-21 16:02:02 -0700

    function that prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: char
+ * Return: 0
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+
+}
