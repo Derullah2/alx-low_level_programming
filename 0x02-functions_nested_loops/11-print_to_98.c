13588a601a63abf9ff0c3da81b96b6f8b444fe52
Derullah2 <ewadyrogati98@gmail.com>
2023-03-22 14:31:17 -0700

    function to ptint 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * void print_to_98 - prints all natural numbers
+ * @n: interger
+ * Return: Always zero
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--);
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+
+}
