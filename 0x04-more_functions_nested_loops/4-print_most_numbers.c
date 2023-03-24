cec47fd6318e20d336c9c2056f38e871d0303bed
Derullah2 <ewadyrogati98@gmail.com>
2023-03-24 14:52:31 -0700

    i belive in numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int x;
+	for (; x <= 0; X++)
+	{
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	}
+
+	_putchar('\n');
+}
