c3810a202bf4da03227287c27e61638af3011cce
Derullah2 <ewadyrogati98@gmail.com>
2023-03-26 14:49:05 -0700

    seven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,7 @@
 #include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_line(0);
-    print_line(2);
-    print_line(10);
-    print_line(-4);
-    return (0);
-}
-
-
-7-print_diagonal.c
-
-#include "main.h"
-
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
- * return: void
  */
 
 void print_diagonal(int n)
