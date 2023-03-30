8e8f50846c42c472a851f9080d0efd752590435d
Derullah2 <ewadyrogati98@gmail.com>
2023-03-30 07:44:41 -0700

    a function that copies a string.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#inlcude "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: string
+ * @n: interger
+ * Wrighten By Derullah
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+return (0);
+}
