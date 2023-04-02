92a7680f0806292952c55e400987ad75333e9b14
Derullah2 <ewadyrogati98@gmail.com>
2023-04-02 03:08:19 -0700

    magi

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,18 +4,20 @@
 * @n: integer params
 * Return: 0
 */
-void print_number(int n)
+int main(void)
 {
-unsigned int n1;
-n1 = n;
-if (n < 0)
-{
-putchar('-');
-n1 = -n;
-}
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-putchar((n1 % 10) + '0');
+	print_number(98);
+	putchar('\n');
+	print_number(402);
+	putchar('\n');
+	print_number(1024);
+	putchar('\n');
+	print_number(0);
+	putchar('\n');
+	print_number(-98);
+	putchar('\n');
+	return (0);
+
+
+
 }
