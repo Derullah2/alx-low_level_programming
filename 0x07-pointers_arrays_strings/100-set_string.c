5be70f50ecf1902ec7f0a8b0afe36e5ec6dcf6a1
Derullah2 <ewadyrogati98@gmail.com>
2023-04-03 16:31:53 -0700

    value of pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - set the value of a pointerto char
+ * @s: the pointer
+ * @to: the char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strstr - locaters substrings
+ * @haystack: string
+ * @needle: function finds the first occurrence of the substring
+ * Return: NUll
+ */
+
