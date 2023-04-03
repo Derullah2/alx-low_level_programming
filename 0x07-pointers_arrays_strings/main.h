b732e77d2152845fa7bd5bb5ee01a3f1018ea536
Derullah2 <ewadyrogati98@gmail.com>
2023-04-03 12:42:02 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
+int _putchar(char c);
+
+#endif
