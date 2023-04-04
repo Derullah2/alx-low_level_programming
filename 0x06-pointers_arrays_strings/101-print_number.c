23bc34b48bbdd25845eab8287a959f7dc1f0b64e
Derullah2 <ewadyrogati98@gmail.com>
2023-04-04 14:27:33 -0700

    prints chess board

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
-* print_number _ prints number
-* @n: interger
+* print_number _ prints number character
+* @n: interger paragams
+* @n1: interger 2
 * Return: 0
 */
+
 void print_number(int n)
 {
 
@@ -20,6 +23,4 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
-
-
 }
