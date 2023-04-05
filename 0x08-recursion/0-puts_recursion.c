deab11c6fa57ef8c4d4c6381a280a860338e6b3f
Derullah2 <ewadyrogati98@gmail.com>
2023-04-05 07:06:59 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,15 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s -- '\0')
 
+	if (*s)
 	{
-	_putchar('\n');
-		return 0;
+	_putchar(*s);
+	_puts_recursion (s + 1);	
 	}
 	else
 	{
-	_putchar(*s);
-	_putchar(*s++);
+	_putchar('\n');
 	}
 
 }
