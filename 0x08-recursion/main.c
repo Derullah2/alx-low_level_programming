e5772b2ea6792ecb9c4dc0c6a4ae072ed6209335
Derullah2 <ewadyrogati98@gmail.com>
2023-04-05 07:28:18 -0700

    reverse

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,14 @@
+julien@ubuntu:~/0x08. Recursion$ cat 0-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
+
