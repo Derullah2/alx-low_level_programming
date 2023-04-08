7bd4a6e6f30227a347db3f80f923d8d288e8900e
Derullah2 <ewadyrogati98@gmail.com>
2023-04-08 14:08:57 -0700

    a program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main -main entry
- * @argc: arguement count
- * @argv: arguement vector
- * Return: EXIT_SUCCESS
+ * main - main entry
+ * @agrv[]: arguement vectort
+ * @agrc: arguement count
+ * Return: 0
+ * WRIGHTEN BY DERULLAH
  */
-int main(int argc, char *argv[])
+int  main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	printf("%d\n", argc);
-	return (EXIT_SUCCESS);
+	int i;
+	for (i = 0; 1 < argc; i++)
+	{
+	printf("%s\n", argv[1]);
+	}
+	return (0);
+
 }
