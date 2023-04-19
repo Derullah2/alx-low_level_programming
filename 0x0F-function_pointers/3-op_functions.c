ae9f85f7745a0d77cbd6fc3d7c52e5830e4a7dcb
Derullah2 <ewadyrogati98@gmail.com>
2023-04-19 16:10:49 -0700

    all

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - add opperator
+ * @a: interger 1
+ * @b: interger 2
+ * Return: NULL
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - add opperator
+ * @a: interger 1
+ * @b: interger 2
+ * Return: NULL
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - add opperator
+ * @a: interger 1
+ * @b: interger 2
+ * Return: NULL
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * op_mul - add opperator
+ * @a: interger 1
+ * @b: interger 2
+ * Return: NULL
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * op_mul - add opperator
+ * @a: interger 1
+ * @b: interger 2
+ * Return: NULL
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,3 @@
-
 #ifndef _HEADER_
 #define _HEADER_
 #include <stdlib.h>
