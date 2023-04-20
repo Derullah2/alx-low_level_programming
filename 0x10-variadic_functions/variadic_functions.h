1879373f802f34c0e53c764d8d584cde5ba55d7e
Derullah2 <ewadyrogati98@gmail.com>
2023-04-20 15:25:57 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
