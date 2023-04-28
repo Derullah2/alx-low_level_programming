cd11c8ea49293896e115b84b219dbce270940624
Derullah2 <ewadyrogati98@gmail.com>
2023-04-28 10:14:34 -0700

    a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,29 +3,62 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int _stringlenth(const char *d);
+void *_strdup(const char *src);
+
 /**
  * add_node - addes nodes to the program
  * @head: head of node
  * @str: string to work with
  * Return: NULL
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-{
-return (NULL);
+	list_t *newhead;
+
+	newhead = malloc(sizeof(list_t));
+	if (newhead == NULL)
+		return (NULL);
+
+	newhead->len = _strlen(str);
+	newhead->str = _strdup(str);
+	newhead->next = *head;
+	*head = newhead;
+	return (newhead);
 }
 
-strncpy = strdup(str);
- if (strncpy == NULL)
+/**
+ * _strlen - prints out the  string lenths
+ * @s: string to print on the program
+ * Return: strlen
+ */
+int _stringlenth(const char *s)
 {
-free(new_node);
-return NULL;
-}
-new_node->str = strncpy;
-new_node->next = *head;
-*head = new_node;
+	int con;
 
-return (new_node);
+	for (con = 0; s[con]; con++)
+		;
+	return (con);
+}
+
+/**
+ * _strdup - funct to duplicate string from src to dest
+ * @src: string to be duplicated is here
+ * Return: destination of  copied string
+ */
+void *_strdup(const char *src)
+{
+	int lengths;
+	int con;
+	char *dest;
+
+	lengths = _stringlenth(src);
+	dest = malloc((lengths + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (con = 0; src[con]; con++)
+		dest[con] = src[con];
+	dest[con] = '\0';
+	return (dest);
 }
