15a272767b0929440630b541aaddafac866eb163
Derullah2 <ewadyrogati98@gmail.com>
2023-05-02 04:13:47 -0700

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,34 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t LoopedListintLen(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * LoopedListintLen - Counts the number of  nodes
+ * in a looped listint_t lnkd list in tye prpogram
+ * @head: the header to be ude in the program.
+ *
+ * Return: 0.
+ */
+size_t LoopedListintLen(const listint_t *head)
+{
+const listint_t *tortoise, *hare;
+size_t nod = 1;
+
+if (head == NULL || head->next == NULL)
+return (0);
+
+tortoise = head->next;
+hare = (head->next)->next;
+
+while (hare)
+{
+if (tortoise == hare)
+{
+tortoise = head;
+while (tortoise != hare)
+{
+nod++;
+tortoise = tortoise->next;
+hare = hare->next;
+}
+tortoise = tortoise->next;
+while (tortoise != hare)
+{
+nod++;
+tortoise = tortoise->next;
+}
+
+return (nod);
+}
+
+tortoise = tortoise->next;
+hare = (hare->next)->next;
+}
+
+return (0);
+}
+
 /**
  * delete_nodeint_at_index - function that delt the nod at index in the program
  * @head: the header to be used in the program
  * @indext: the indext to be deleted
  * Return: 1 if it succeeded else -1 if it failed
  */
+
 size_t print_listint_safe(const listint_t *head)
 {
-size_t nds, ind = 0;
+size_t nod, ind = 0;
 
-nds = LoopedListintLen(head);
+nod = LoopedListintLen(head);
 
-if (nds == 0)
+if (nod == 0)
 {
-for (; head != NULL; nds++)
-
+for (; head != NULL; nod++)
 {
-
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
 }
 
 else
-
 {
-for (ind = 0; ind < nds; ind++)
-
+for (ind = 0; ind < nod; ind++)
 {
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
@@ -38,6 +84,5 @@ head = head->next;
 printf("-> [%p] %d\n", (void *)head, head->n);
 }
 
-return (nds);
+return (nod);
 }
-
