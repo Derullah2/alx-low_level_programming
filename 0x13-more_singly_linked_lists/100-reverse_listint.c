507cd2266508e7482a7a919cb7b26024c1799501
Derullah2 <ewadyrogati98@gmail.com>
2023-05-02 03:52:48 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,86 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t LoopedListintLen(const listint_t *head);
-size_t printlistsafe(const listint_t *head);
-
 /**
- * LoopedListintLen - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A Pointer to the header of the listint to chec
- * Return: If the list is not looped - 0.
- */
-size_t LoopedListintLen(const listint_t *head)
-{
-const listint_t *tortoise, *hare;
-size_t nodes = 1;
-
-if (head == NULL || head->next == NULL)
-return (0);
-
-tortoise = head->next;
-hare = (head->next)->next;
-
-while (hare)
-{
-if (tortoise == hare)
-{
-tortoise = head;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-hare = hare->next;
-}
-
-tortoise = tortoise->next;
-while (tortoise != hare)
-{
-nodes++;
-tortoise = tortoise->next;
-}
-
-return (nodes);
-}
-
-tortoise = tortoise->next;
-hare = (hare->next)->next;
-}
-
-return (0);
-}
-
-/**
- * printlistsafe - Prints a listint
- * @head: A pointer to the head of the listint
+ * reverse_listint - reverses a linked list in the program
+ * @head: the header tio work with in the program
  *
- * Return: The number of nodes in the list
+ * Return: node in the new list
  */
-size_t printlistsafe(const listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-size_t nodes, index = 0;
+listint_t *previous = NULL;
+listint_t *nextinline = NULL;
 
-nodes = LoopedListintLen(head);
-
-if (nodes == 0)
+while (*head)
 {
-for (; head != NULL; nodes++)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-}
+nextinline = (*head)->nextinline;
+(*head)->nextinline = previous;
+previous = *head;
+*head = nextinline;
 }
 
-else
-{
-for (index = 0; index < nodes; index++)
-}
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
-{
+*head = previous;
 
-printf("-> [%p] %d\n", (void *)head, head->n);
-}
-
-return (nodes);
+return (*head);
 }
