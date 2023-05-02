87fbef4a442bda6a73a8d912f2cfc7c4315f13e5
Derullah2 <ewadyrogati98@gmail.com>
2023-05-02 05:21:13 -0700

    new all

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function of the dogs description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: Owner of the dog
+ */
+
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/* The protypes*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
